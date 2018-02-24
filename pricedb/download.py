a02b4c3f969e2b1944a5329383e8a325ee5371b4
Alen Siljak <alen.siljak@gmx.com>
2018-02-24 23:43:11 +0100

    namespace validation for vanguard

--- pricedb/download.py
+++ pricedb/download.py
@@ -170,6 +170,15 @@ class AlphaVantageDownloader:
         self.logger.debug(f"{latest_key}, {result}")
         return result
 
+
 class VanguardAuDownloader:
-    """ Downloads prices from Vanguard Australia """
-    pass
+    """
+    Downloads prices from Vanguard Australia
+    """
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def download(self, namespace: str, symbol: str, currency: str):
+        """ Download price """
+        if namespace != "Vanguard":
+            raise ValueError("Only Vanguard namespace is handled by this agent.")
