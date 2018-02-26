4cf3375e7d8342927039c3a9c44dfce171ded477
Alen Siljak <alen.siljak@gmx.com>
2018-02-26 17:43:24 +0100

    fixed formatting to align the prices on import

--- pricedb/model.py
+++ pricedb/model.py
@@ -15,4 +15,6 @@ class PriceModel:
     def __repr__(self):
         symbol = f"{self.namespace}:{self.symbol}" if self.namespace else self.symbol
         symbol = f"{symbol:<13}"
-        return f"<Price ('{symbol}',date:{self.datetime},value:{self.value},currency:{self.currency})>"
+
+        value = f"{self.value:>6}"
+        return f"<Price ('{symbol}',date:{self.datetime},value:{value},currency:{self.currency})>"
