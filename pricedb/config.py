fe615b59917afce5c2c434fe71087b6e9cf3cc5a
Alen Siljak <alen.siljak@gmx.com>
2018-03-03 19:44:10 +0100

    adding newline

--- pricedb/config.py
+++ pricedb/config.py
@@ -74,6 +74,7 @@ class Config:
         """ Copy the config template into user's directory """
         src_path = self.__get_config_template_path()
         src = os.path.abspath(src_path)
+
         if not os.path.exists(src):
             message = f"Config template not found {src}"
             self.logger.error(message)
