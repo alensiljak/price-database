3ffdc60e0c37e866aec6a7b7a2a2dd59424e819b
Alen Siljak <alen.siljak@gmx.com>
2018-06-14 13:04:52 +0200

    initial download test, for debugging

--- tests/test_vanguard_dl.py
+++ tests/test_vanguard_dl.py
@@ -1,4 +1,10 @@
 """ Test Vanguard downloader """
+from pricedb import PriceDbApplication
+
+app = PriceDbApplication()
 
 def test_date():
-    
+    """ Test the price date """
+    price = app.download_price("VANGUARD:BOND", "AUD", "vanguard")
+
+    assert price is not None
