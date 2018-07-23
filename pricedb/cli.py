7ae2ff0717e270582b64f036813b585426663f64
Alen Siljak <alen.siljak@gmx.com>
2018-07-23 16:59:33 +0200

    no need for the second check

--- pricedb/cli.py
+++ pricedb/cli.py
@@ -145,8 +145,7 @@ def download(symbol: str, namespace: str, file: str, agent: str, currency: str):
     # TODO: pass the other filters
 
     # Otherwise download the prices for securities listed in the database.
-    if file is None:
-        app.download_prices_in_db(currency=currency)
+    app.download_prices_in_db(currency=currency)
 
     #print("Please use --symbol or --file option. --help for more info. Symbol will have preference over file.")
 
