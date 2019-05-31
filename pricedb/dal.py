941724ef61a9a54fcfe0fa7859a2f6bc8ec6e305
Alen Siljak <462445+alensiljak@users.noreply.github.com>
2019-05-31 13:28:55 +0200

    removing unused entity/table

--- pricedb/dal.py
+++ pricedb/dal.py
@@ -36,15 +36,15 @@ class Price(Base):
         return f"<Price ({formatted_symbol},{self.date} {self.time},{actual_value_str:>6})>"
 
 
-class SymbolMap(Base):
-    """ Map between in- and out- symbols """
-    __tablename__ = "symbol_map"
+# class SymbolMap(Base):
+#     """ Map between in- and out- symbols """
+#     __tablename__ = "symbol_map"
 
-    in_symbol = Column(String, primary_key=True)
-    out_symbol = Column(String)
+#     in_symbol = Column(String, primary_key=True)
+#     out_symbol = Column(String)
 
-    def __repr__(self):
-        return f"<SymbolMap (in={self.in_symbol},out={self.out_symbol})>"
+#     def __repr__(self):
+#         return f"<SymbolMap (in={self.in_symbol},out={self.out_symbol})>"
 
 
 class Security(Base):
